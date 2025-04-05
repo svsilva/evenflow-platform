9b0a73ad30721784be1217b124372e5ac998dfe9
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-05 17:19:46 -0300

    feat: implementação de autenticação

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const Usuario = require('../models/Usuario');
+
+exports.atualizar = async(req, res, next) => {
+    try{
+        let token;
+
+        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+            token = req.headers.authorization.split(' ')[1];
+        }
+
+        if(!token){
+            return res.status(401).json({ mensagem: 'Não autorizado - Token não fornecido' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const usuario = await Usuario.findByPk(decoded.id);
+
+        if(!usuario){
+            return res.status(401).json({ mensagem: 'Não autorizado - Usuário não encontrado' });
+        }
+
+        req.usuario = usuario;
+        next();
+    }catch(error){
+        return res.status(401).json({ mensagem: 'Não autorizado - Token inválido'});
+    }
+};
+
+exports.restringirPara = (...nivelAcesso) => {
+    return(req, res, next) => {
+        if(!nivelAcesso.includes(req.usuario.nivelAcesso)){
+            return res.status(403).json({ mensagem: 'Você não tem permissão para realizar esta ação' });
+        }
+    }
+};
