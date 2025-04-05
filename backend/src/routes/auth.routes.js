bb61039f134a1c4f380a3f2db5588138e315601f
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-05 17:36:06 -0300

    feat: implementação de rota de autenticação

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const AuthController = require('../controllers/authController');
+const { autenticar } = require('../middlewares/auth');
+
+//Rotas de autenticação
+router.post('/login', AuthController.login);
