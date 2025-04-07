f5055ff27e9a7a4938f10bd0568860e424a8f4ca
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-07 15:10:49 -0300

    feat: inserção de rota post para criar local

---
+++ backend/src/routes/local.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const localController = require('../controllers/localController');
+const { validarLocal } = require('../validators/localValidators');
+const autenticar = require('../middlewares/auth');
+
+//Rotas públicas
+
+
+//Rotas protegidas
+router.post('/', autenticar, validarLocal, localController.criarLocal);
+
+module.exports = router;
