1afc21a77e3c7c780980966c1b197196bac3573b
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-10 09:50:09 -0300

    feat: implementação de relacionamento evento, ingresso e usuário

--- backend/src/models/associations/associations.js
+++ backend/src/models/associations/associations.js
@@ -1,37 +1,25 @@
+const Usuario = require('../Usuario');
 const Evento = require('../Evento');
 const Local = require('../Local');
-const Usuario = require('../Usuario');
+const Ingresso = require('../Ingresso');
+
 
 const setupAssociations = () => {
     //Relacionamento entre Evento e Local
-    Evento.belongsTo(Local, {
-        as: 'local',
-        foreignKey: 'localId',
-        onDelete: 'SET NULL',
-        onUpdate: 'CASCADE'
-    });
-
-    Local.hasMany(Evento, {
-        as: 'eventos',
-        foreignKey: 'localId',
-        onDelete: 'SET NULL',
-        onUpdate: 'CASCADE'
-    });
+    Evento.belongsTo(Local, { as: 'local', foreignKey: 'localId', onDelete: 'SET NULL', onUpdate: 'CASCADE' });
+    Local.hasMany(Evento, { as: 'eventos', foreignKey: 'localId', onDelete: 'SET NULL', onUpdate: 'CASCADE' });
 
     //Relacionamento entre Evento e Usuário
-    Evento.belongsTo(Usuario, {
-        as: 'organizador',
-        foreignKey: 'organizadorId',
-        onDelete: 'SET NULL',
-        onUpdate: 'CASCADE'
-    });
+    Evento.belongsTo(Usuario, { as: 'organizador', foreignKey: 'organizadorId', onDelete: 'SET NULL', onUpdate: 'CASCADE' });
+    Usuario.hasMany(Evento, { as: 'eventos', foreignKey: 'organizadorId', onDelete: 'SET NULL', onUpdate: 'CASCADE' });
 
-    Usuario.hasMany(Evento, {
-        as: 'eventos',
-        foreignKey: 'organizadorId',
-        onDelete: 'SET NULL',
-        onUpdate: 'CASCADE'
-    });
+    //Relacionamento entre Evento e Ingresso
+    Ingresso.belongsTo(Evento, { as: 'evento', foreignKey: 'eventoId', onDelete: 'SET NULL', onUpdate: 'CASCADE' });
+    Evento.hasMany(Ingresso, { as: 'ingressos', foreignKey: 'eventoId', onUpdate: 'SET NULL', onDelete: 'CASCADE' });
+
+    //Relacionamento entre Ingresso e Usuário
+    Ingresso.belongsTo(Usuario, { as: 'comprador', foreignKey: 'compradorId', onUpdate: 'SET NULL', onDelete: 'CASCADE' });
+    Usuario.hasMany(Ingresso, { as: 'ingressosComprados', foreignKey: 'compradorId', onUpdate: 'SET NULL', onDelete: 'CASCADE' });
 
 };
 
