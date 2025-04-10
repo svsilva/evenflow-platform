b21619e5536d2b8c9385d5bf3a4fe91edf91ae23
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-10 09:40:13 -0300

    feat: implementação de model Ingresso

--- backend/src/models/Ingresso.js
+++ backend/src/models/Ingresso.js
@@ -1,1 +1,57 @@
-const { DataTypes } = require
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+const Evento = require('./Evento');
+const Usuario = require('./Usuario');
+
+const Ingresso = sequelize.define('Ingresso', {
+    id:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    codigo:{
+        type: DataTypes. STRING,
+        allowNull: false,
+        unique: true
+    },
+    preco:{
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    status:{
+        type: DataTypes.ENUM('disponivel', 'reservado', 'vendido', 'utilizado', 'cancelado'),
+        defaultValue: 'disponivel',
+        allowNull: false
+    },
+    tipo:{
+        type: DataTypes.ENUM('inteira', 'meia', 'promocional'),
+        defaultValue: 'inteira',
+        allowNull: false
+    },
+    dataCompra:{
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    dataUtilização:{
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    eventoId:{
+        type: DataTypes.UUID,
+        allowNull: false,
+        references:{
+            model: Evento,
+            key: 'id'
+        }
+    },
+    compradorId:{
+        type: DataTypes.UUID,
+        allowNull: false,
+        references:{
+            model: Usuario,
+            key: 'id'
+        }
+    }
+});
+
+module.exports = Ingresso;
