ee4f89dcef9e88ca4d686b11ac53d1a4207b7c05
igor bertelli <igor.bertelli@mail.usf.edu.br>
2025-04-20 20:01:52 -0300

    fix: ajuste na resposta ao criar sessão de checkout

--- backend/src/controllers/checkoutSessionController.js
+++ backend/src/controllers/checkoutSessionController.js
@@ -46,16 +46,13 @@ class CheckoutSessionController {
                 ingressoId
             });
 
-            res.status(201).json({
-                mensagem: 'Sessão de checkout criada com sucesso!',
-                checkoutSession: novaCheckoutSession
-            });
+            res.status(201).json(novaCheckoutSession );
         } catch (error) {
             res.status(500).json({ mensagem: 'Erro ao criar sessão de checkout', erro: error.message });
         }
     }
 
-    // Atualizar o status da sessão de checkout
+    // rota para atualizar o status da sessão de checkout apenas no banco de dados (Não esta em uso por enquanto)
     async atualizarStatusCheckout(req, res) {
         try {
             const { id } = req.params;
