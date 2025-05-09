a1d890669034cba745537772d1557aad4c014682
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-05-09 19:08:29 -0300

    refactor: correção nas chamadas dos temas

--- frontend/src/styles/themes/darkTheme.js
+++ frontend/src/styles/themes/darkTheme.js
@@ -1,7 +1,15 @@
 import { createTheme } from '@mui/material/styles';
+import { colors } from './colors';
 
 export const darkTheme = createTheme({
     palette:{
         mode: 'dark',
-    }
+        primary: { ...colors.primary },
+        secondary: { ...colors.secondary },
+        background: { ...colors.background },
+        text: { ...colors.text },
+        error: { main: colors.error },
+        success: { main: colors.success},
+        warning: { main: colors.warning }
+    },
 });
